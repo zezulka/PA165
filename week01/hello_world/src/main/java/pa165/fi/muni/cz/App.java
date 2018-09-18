6beed4e03112162583d600f69d1262a343fa7305
Miloslav Žežulka <zezulka95@gmail.com>
2018-09-18 13:38:28 +0200

    Commit of the first week.

---
+++ week01/hello_world/src/main/java/pa165/fi/muni/cz/App.java
@@ -0,0 +1,13 @@
+package pa165.fi.muni.cz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA165!" );
+    }
+}
---
+++ week01/hello_world/src/test/java/pa165/fi/muni/cz/AppTest.java
@@ -0,0 +1,20 @@
+package pa165.fi.muni.cz;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
